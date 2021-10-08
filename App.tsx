aeb8038c001f4b8496557acd34bf070200bad7f3
Lucca Dumas <lucca.ribeiro0@hotmail.com>
2021-10-08 01:59:18 -0300

    feat(methods): Adding POST, PUT, DELETE methods

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   const getUser = async () => {
@@ -19,13 +19,48 @@ export default function App() {
     }
   };
 
-  useEffect(() => {
-    getUser();
-  }, []);
+  const postUser = async () => {
+    try {
+      const response = await fetch("", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstParam: "",
+          secondParam: "",
+        }),
+      });
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handlerPut = async () => {
+    console.log("PUT");
+  };
+
+  const handlerDelete = async () => {
+    console.log("DELETE");
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity style={styles.button} onPress={() => getUser()}>
+        <Text style={styles.buttonText}>GET</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => postUser()}>
+        <Text style={styles.buttonText}>POST</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => handlerPut()}>
+        <Text style={styles.buttonText}>PUT</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => handlerDelete()}>
+        <Text style={styles.buttonText}>DELETE</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -37,5 +72,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    width: "90%",
+    height: 60,
+    backgroundColor: "#3498db",
+    marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 26,
+    color: "#FFF",
+    textAlign: "center",
   },
 });
